6e3969263ecea51900b515a3b59ca55380f3a96b
httpo <uhagodqhr94@naver.com>
2024-01-07 11:23:21 +0900

    [FEAT] dockerfile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
     <div className="text-center my-10">
-      <div className="text-3xl font-bold">SideHubFront</div>
+      <div
+        className="w-96 mx-auto rounded py-2"
+        style={{ backgroundColor: "#2D3250" }}
+      >
+        <div
+          className="w-80 mx-auto rounded py-2"
+          style={{ backgroundColor: "#424769" }}
+        >
+          <div
+            className="w-72 mx-auto rounded py-2"
+            style={{ backgroundColor: "#7077a1" }}
+          >
+            <div className="text-3xl font-bold" style={{ color: "#f6b17a" }}>
+              SideHubFront
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
